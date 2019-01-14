948e88ddfd1dcd70eb99db85050f5cb646e76eef
SDatsyuk <sdatsyuk14741@gmail.com>
2019-01-14 12:09:48 +0200

    realtime inference object direction add

--- nn_inference/realtime_inference.py
+++ nn_inference/realtime_inference.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import numpy as np
 import cv2
+from collections import deque
 
 import label_map_util
 import visualization_utils as vis_util
@@ -12,8 +13,14 @@ ap.add_argument("--input", default=0, help="video input or camera index [default
 ap.add_argument('--model', required=True, help='path to model file')
 ap.add_argument("--labels", required=True, help='path to label pbtxt file')
 ap.add_argument("--num_classes", required=True, help='number of classes')
+ap.add_argument("-t", "--threshold", default=0.5)
+ap.add_argument("-b", "--direction_buffer", type=int, default=32,
+	help="max direction buffer size")
 
 args = vars(ap.parse_args())
+pts = deque(maxlen=args['direction_buffer'])
+counter = 0
+direction = ""
 
 def load_model(path, sess):
 	
@@ -84,6 +91,49 @@ def run_inference_for_single_image(image, graph, sess):
 	return output_dict
 
 
+def find_direction(frame, pts):
+	global counter
+	direction = ""
+	for i in np.arange(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+ 
+		# check to see if enough points have been accumulated in
+		# the buffer
+		if counter >= 10 and i == 1 and pts[-10] is not None:
+			# compute the difference between the x and y
+			# coordinates and re-initialize the direction
+			# text variables
+			dX = pts[-10][0] - pts[i][0]
+			dY = pts[-10][1] - pts[i][1]
+			(dirX, dirY) = ("", "")
+ 
+			# ensure there is significant movement in the
+			# x-direction
+			if np.abs(dX) > 20:
+				dirX = "to basket" if np.sign(dX) == 1 else "to package"
+ 
+			# ensure there is significant movement in the
+			# y-direction
+			# if np.abs(dY) > 20:
+			# 	dirY = "North" if np.sign(dY) == 1 else "South"
+ 
+			# handle when both directions are non-empty
+			direction = dirX
+ 
+			# otherwise, only one direction is non-empty
+			# else:
+				# direction = dirX if dirX != "" else dirY
+
+		cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+		0.65, (0, 0, 255), 3)
+		thickness = int(np.sqrt(args["direction_buffer"] / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+	counter += 1
+
+
 def inference():
 
 	sess = tf.Session()
@@ -92,10 +142,24 @@ def inference():
 
 	cap = cv2.VideoCapture(args['input'])
 
+	h, w = cap.get(cv2.CAP_PROP_FRAME_HEIGHT), cap.get(cv2.CAP_PROP_FRAME_WIDTH )
+	print(h, w)
+
 	while True:
 		_, frame = cap.read()
 
 		output_dict = run_inference_for_single_image(frame, detection_graph, sess)
+
+		obj = [output_dict['detection_boxes'][i] for i in range(output_dict['num_detections']) if output_dict['detection_scores'][i] > args['threshold']]
+		if obj:
+			obj = obj[0]
+			# print(obj[2] * w / 2, obj[3] * h / 2)
+			# cv2.circle(frame, (int((obj[3] - (obj[3] - obj[1]) / 2) * w) ,int((obj[2] - (obj[2] - obj[0]) / 2) * h)), 5, (0, 0, 255), -1)
+			center = (int((obj[3] - (obj[3] - obj[1]) / 2) * w) ,int((obj[2] - (obj[2] - obj[0]) / 2) * h))
+				# print(objects)
+			pts.appendleft(center)
+
+			find_direction(frame, pts)
 
 		vis_util.visualize_boxes_and_labels_on_image_array(
 			frame,
@@ -106,7 +170,7 @@ def inference():
 			instance_masks=output_dict.get('detection_masks'),
 			use_normalized_coordinates=True,
 			line_thickness=8, 
-			min_score_thresh=.3
+			min_score_thresh=args['threshold']
 			)
 
 		cv2.imshow('reco', frame)
